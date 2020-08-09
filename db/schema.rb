eaf17f0d6ce2756e18012152f1eb880929200618
Sasho1989 <caki.jova@gmail.com>
2020-08-09 18:06:44 +0200

    Fixed typo for recipe attribute

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_154844) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
-    t.text "desciption"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

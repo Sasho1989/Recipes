626a615afb905985f106d659c52bef7f2bc2c1c8
Sasho1989 <caki.jova@gmail.com>
2020-08-09 19:53:06 +0200

    Option to create and save a Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,21 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+
+    redirect_to recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
 end

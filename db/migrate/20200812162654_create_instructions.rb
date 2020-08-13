4e083ed0290d3637a80b72c2aae82eab5a24c4bd
Sasho1989 <caki.jova@gmail.com>
2020-08-13 11:49:41 +0200

    Fixed Recipe_id to recipe_id in migration

--- db/migrate/20200812162654_create_instructions.rb
+++ db/migrate/20200812162654_create_instructions.rb
@@ -1,7 +1,7 @@
 class CreateInstructions < ActiveRecord::Migration[6.0]
   def change
     create_table :instructions do |t|
-      t.references :Recipe, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
 
       t.timestamps
     end

54362d8695922b2d8964fa69bd9de2f186239a03
Sasho1989 <caki.jova@gmail.com>
2020-08-14 21:08:13 +0200

    Associating ingredients model with Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,9 @@ class Recipe < ApplicationRecord
   has_many :instructions, dependent: :destroy
   accepts_nested_attributes_for :instructions, reject_if: :all_blank, allow_destroy: true
 
+  has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+
   validates :title, presence: true, length: { minimum: 4 }
   validates :description, presence: true, length: { maximum: 255 }
 end

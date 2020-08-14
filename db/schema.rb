1cfee6a39a51f21e64da44c2f3b3a148bc1c425d
Sasho1989 <caki.jova@gmail.com>
2020-08-14 20:59:31 +0200

    New Ingredient model with reference to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_183121) do
+ActiveRecord::Schema.define(version: 2020_08_14_185514) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.text "component"
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "instructions", force: :cascade do |t|
     t.integer "recipe_id", null: false
@@ -27,5 +35,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_183121) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "instructions", "recipes"
 end

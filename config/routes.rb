47104eed0a0e647c13f29e8223bfb885d20e9213
Sasho1989 <caki.jova@gmail.com>
2020-08-17 11:08:57 +0200

    New users controller with option to create and save user

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
     resources :instructions
     resources :ingredients
   end
+
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
